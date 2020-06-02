55594a0fdac721888faeaafc7945b343c8aeb90b
ucwong <ucwong@126.com>
2020-06-02 02:21:27 +0000

    handler_test | available and get file test fix

--- handler_test.go
+++ handler_test.go
@@ -4,15 +4,21 @@ import (
 	"fmt"
 	"log"
 	"testing"
+	"time"
 )
 
 func TestGetFile(t *testing.T) {
-	tm := NewTorrentManager(DefaultConfig{}, 1, true, false)
+	DefaultConfig.DataDir = "/root/.cortex/storage"
+	fmt.Println(DefaultConfig)
+	tm, _ := NewTorrentManager(&DefaultConfig, 1, true, false)
 	tm.Start()
 	defer tm.Close()
-	file := tm.GetFile("3f1f6c007e8da3e16f7c3378a20a746e70f1c2b0", "data")
+	time.Sleep(10 * time.Second)
+	a, _ := tm.Available("3f1f6c007e8da3e16f7c3378a20a746e70f1c2b0", 100000000)
+	fmt.Println("available", a)
+	file, _ := tm.GetFile("3f1f6c007e8da3e16f7c3378a20a746e70f1c2b0", "data")
 	if file == nil {
 		log.Fatal("failed to get file")
 	}
-	fmt.Println("file", file)
+	fmt.Println("file", file[:20])
 }
