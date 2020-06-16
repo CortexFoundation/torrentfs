8a99f49e949a8e1a7913f0d4898cc6ce294750d3
ucwong <ucwong@126.com>
2020-06-16 13:15:39 +0800

    mkt test fix

--- merkletree/merkle_tree_test.go
+++ merkletree/merkle_tree_test.go
@@ -47,7 +47,7 @@ func TestNewTree(t *testing.T) {
 		t.Error("new tree error: ", err)
 	}
 	rootHash := common.BytesToHash(root.Root.Hash).Hex()
-	want := "0x90f4b39548df55ad6187a1d20d731ecee78c545b94afd16f42ef7592d99cd365"
+	want := "0xa6eef7e35abe7026729641147f7915573c7e97b47efa546f5f6e3230263bcb49"
 	if rootHash != want {
 		t.Errorf("root unmatched. should be %s, got %s", want, rootHash)
 	}
