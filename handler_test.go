9941a30718af00dd398707ef311abcdca72a198e
ucwong <ucwong@126.com>
2020-07-15 15:43:32 +0800

    fix handler test

--- handler_test.go
+++ handler_test.go
@@ -13,7 +13,7 @@ func TestGetFile(t *testing.T) {
 	ih := "aea5584d0cd3865e90c80eace3bfcb062473d966"
 	fmt.Println(DefaultConfig)
 	tm, _ := NewTorrentManager(&DefaultConfig, 1, false, false)
-	tm.Search(ih)
+	tm.Search(ih, 0)
 	tm.Start()
 	defer tm.Close()
 	time.Sleep(3 * time.Second)
