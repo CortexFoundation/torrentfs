6bb101e96352ffb00afd995e6db3cfaa38af128e
ucwong <ucwong@126.com>
2020-07-30 21:11:33 +0800

    torrent channel buf

--- handler.go
+++ handler.go
@@ -50,10 +50,10 @@ import (
 )
 
 const (
-	bucket = params.Bucket //it is best size is 1/3 full nodes
-	group  = params.Group
-	//updateTorrentChanBuffer = params.SyncBatch
-	torrentChanSize = 64
+	bucket                  = params.Bucket //it is best size is 1/3 full nodes
+	group                   = params.Group
+	updateTorrentChanBuffer = params.SyncBatch
+	torrentChanSize         = 64
 
 	torrentPending = iota //2
 	torrentPaused
@@ -399,7 +399,7 @@ func NewTorrentManager(config *Config, fsid uint64, cache, compress bool) (*Torr
 		TmpDataDir:          tmpFilePath,
 		boostFetcher:        NewBoostDataFetcher(config.BoostNodes),
 		closeAll:            make(chan struct{}),
-		updateTorrent:       make(chan interface{}), // updateTorrentChanBuffer),
+		updateTorrent:       make(chan interface{}, updateTorrentChanBuffer),
 		seedingChan:         make(chan *Torrent, torrentChanSize),
 		activeChan:          make(chan *Torrent, torrentChanSize),
 		pendingChan:         make(chan *Torrent, torrentChanSize),
