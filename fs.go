8538b45b8bb90c9e50925a1af9ea97dac062a538
ucwong <ucwong@126.com>
2020-08-03 16:39:48 +0800

    nas invoke protocol

--- fs.go
+++ fs.go
@@ -255,7 +255,8 @@ func (fs *TorrentFS) Available(ctx context.Context, infohash string, rawSize uin
 					t := float64(cost) / (1000 * 1000 * 1000)
 					speed = float64(f) / t
 				}
-				if ProtocolVersion == 2 && f < rawSize && time.Duration(cost) > time.Second*60 && speed < 256*1024 {
+				invoke := time.Duration(cost) > time.Second*60 || (time.Duration(cost) > time.Second*30 && f == 0)
+				if ProtocolVersion == 2 && f < rawSize && invoke && speed < 256*1024 {
 					go func() {
 						log.Error("Nas 2.0 query", "ih", infohash, "cap", cap(fs.queryChan), "len", len(fs.queryChan), "available", ret, "raw", rawSize, "finish", f, "cost", common.PrettyDuration(cost), "speed", common.StorageSize(speed), "err", err)
 						fs.queryChan <- Query{Hash: infohash, Size: rawSize}
