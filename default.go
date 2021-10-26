5ca1d8d618b9186f39c240a6cdcbd882adec0eb8
ucwong <ucwong@126.com>
2021-10-26 16:41:13 +0800

    tcp enable

--- default.go
+++ default.go
@@ -53,7 +53,7 @@ var DefaultConfig = Config{
 	Mode:            "default",
 	DisableUTP:      false,
 	DisableDHT:      false,
-	DisableTCP:      true,
+	DisableTCP:      false,
 	DisableIPv6:     false,
 	MaxSeedingNum:   params.LimitSeeding / 2,
 	MaxActiveNum:    params.LimitSeeding / 2,
