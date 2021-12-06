e373c41a9304b6ca9da336410903f201bc5d1387
ucwong <ucwong@126.com>
2021-12-06 14:27:02 +0800

    fs current status

--- handler.go
+++ handler.go
@@ -713,7 +713,7 @@ func (tm *TorrentManager) init() {
 	//TODO sync initialize
 	select {
 	case <-tm.initCh:
-		log.Info("Chain files sync init OK !!!")
+		log.Info("Chain files sync init OK !!!", "seeding", len(tm.seedingTorrents), "pending", len(tm.pendingTorrents), "active", len(tm.activeTorrents))
 	case <-tm.closeAll:
 		log.Info("Init files closed")
 		return
